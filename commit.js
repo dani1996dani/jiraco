9a4d2218d7f99c2a874b3cab7626853766a85408
Daniel Baron <danielb@proftit.com>
2020-11-09 16:54:43 +0200

    feat 🛠️	refactored index

---
+++ commit.js
@@ -0,0 +1,28 @@
+const { exec } = require('child_process');
+const chalk = require('chalk');
+const { askQuestion } = require('./ask-question');
+const { getCommitPrefix } = require('./get-commit-prefix');
+const { printError } = require('./print-error');
+
+const commit = () => {
+    exec('git rev-parse --abbrev-ref HEAD', async(err, branchName, stderr) => {
+        if (err) {
+            printError(stderr);
+        }
+        const commitPrefix = getCommitPrefix(branchName);
+        const commitMessage = await askQuestion(chalk.bold.blueBright( "Your commit message: "));
+
+        const fullCommitMessage = `${commitPrefix}\t${commitMessage}`;
+        exec(`git commit -m "${fullCommitMessage}"`, (err, result, stderr) => {
+            if (err){
+                printError(stderr);
+            }
+            // will print default git commit message (how many lines were inserted or deleted)
+            console.log(result);
+        });
+    });
+};
+
+module.exports = {
+    commit
+};
